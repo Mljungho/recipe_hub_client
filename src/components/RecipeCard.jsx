353b51f37be950a63066fdd75882e0625da59acd
Mljungho <mathias.ljungholm@gmail.com>
2022-02-15 11:41:30 +0100

    Adding MUI styling with dop down effect for recipe cards

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -7,11 +7,31 @@ import {
   Typography,
   Avatar,
   colors,
-  CardActionArea
+  CardActionArea,
+  Collapse,
+  IconButton
 } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { styled } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest
+  })
+}));
+
 const RecipeCard = ({ recipe }) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   const navigate = useNavigate();
 
   return (
@@ -19,8 +39,9 @@ const RecipeCard = ({ recipe }) => {
       sx={{ maxWidth: 345, boxShadow: 3 }}
       data-cy={`recipe-card-${recipe.index}`}
     >
-      <CardActionArea onClick={() => navigate(`/recipes/${recipe.id}`)}>
+      <CardActionArea>
         <CardHeader
+          onClick={() => navigate(`/recipes/${recipe.id}`)}
           data-cy={`recipe-header-${recipe.index}`}
           avatar={<Avatar sx={{ bgcolor: colors.red[500] }}>R</Avatar>}
           title={recipe.name}
@@ -31,15 +52,25 @@ const RecipeCard = ({ recipe }) => {
           height="194"
           image="https://mui.com/static/images/cards/paella.jpg"
         />
-        <CardContent>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            data-cy={`recipe-description-${recipe.index}`}
-          >
-            {recipe.instructions}
-          </Typography>
-        </CardContent>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              data-cy={`recipe-description-${recipe.index}`}
+            >
+              {recipe.instructions}
+            </Typography>
+          </CardContent>
+        </Collapse>
       </CardActionArea>
     </Card>
   );
